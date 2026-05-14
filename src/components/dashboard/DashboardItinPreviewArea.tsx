b160e255e93fae1bea2c1948b2416a2982280601
chihoang16 <149895979+hoangchi16@users.noreply.github.com>
2026-05-15 00:24:31 +0700

    feat: added upcoming event section

--- src/components/dashboard/DashboardItinPreviewArea.tsx
+++ src/components/dashboard/DashboardItinPreviewArea.tsx
@@ -1,7 +1,35 @@
 import DashboardItinPreviewCard from "./DashboardItinPreviewCard";
-import { Clock } from "lucide-react";
+import { Clock, PlaneTakeoff, House, Glasses } from "lucide-react";
+
+const iconDict = {
+    Transport: <PlaneTakeoff></PlaneTakeoff>, 
+    Hotel: <House></House>,
+    Sightseeing: <Glasses></Glasses>
+}
 
 export default function DashboardItinPreviewArea() {
+    const tempData = [{
+        name: "Family Trip to Da Nang",
+        location: "Da nang International Airport",
+        category: "Transport",
+        time: "14:00 PM",
+        priority: "High",
+    },
+    {
+        name: "Hotel Check-in",
+        location: "International DaNang Sun Peninsula",
+        category: "Hotel",
+        time: "18:00 PM",
+        priority: "Medium",
+    },
+    {
+        name: "Visit My Khe Beach",
+        location: "My Khe Beach, Da Nang",
+        category: "Sightseeing",
+        time: "19:00 PM",
+        priority: "Low",
+    }] as const;
+    
     return (
         <div>
             <div className="pb-5 pl-3.5 flex flex-row items-center space-x-1.5">
@@ -9,8 +37,22 @@ export default function DashboardItinPreviewArea() {
                 <p className="text-foreground font-bold text-lg">Today is 15/05/2024</p>
             </div>
             
-            <div>
-                <DashboardItinPreviewCard></DashboardItinPreviewCard>
+            <div className="space-y-5">
+                {tempData?.length > 0 ? (
+                    tempData.map((item, index) => (
+                        <DashboardItinPreviewCard
+                        key={index}
+                        name={item.name}
+                        location={item.location}
+                        category={item.category}
+                        time={item.time}
+                        priority={item.priority}
+                        icon={iconDict[item.category]}
+                        />
+                    ))
+                ) : (
+                <p>No items found.</p>
+                )}
             </div>
         </div>
     )
--- src/components/dashboard/DashboardItinPreviewCard.tsx
+++ src/components/dashboard/DashboardItinPreviewCard.tsx
@@ -1,28 +1,43 @@
 import { Card, CardContent } from "../ui/card"
-import { MapPin, PlaneTakeoff, AlarmClock } from "lucide-react"
+import { MapPin, AlarmClock } from "lucide-react"
 import { Badge } from "../ui/badge"
 
-export default function DashboardItinPreviewCard() {
+interface ItinPreviewCardProps {
+  name: string;
+  location: string;
+  category: "Transport" | "Food" | "Sightseeing" | "Shopping"| "Hotel" | "Other";
+  time: string;
+  priority: "Low" | "Medium" | "High";
+  icon: React.ReactNode;
+}
+
+const BadgeVariantDict = {
+    High: "destructive",
+    Medium: "default",
+    Low: "secondary"
+} as const
+
+export default function DashboardItinPreviewCard(props: ItinPreviewCardProps) {
     return (
         <Card className="pl-6">
             <CardContent>
-                <div className="flex flex-row justify-between">
-                    <p className="text-2xl font-bold pb-0.5">Flight to Da Nang</p>
-                    <Badge variant={"destructive"}>High</Badge>
+                <div className="flex flex-row justify-between items-center">
+                    <p className="text-2xl font-bold pb-0.5">{props.name}</p>
+                    <Badge variant={BadgeVariantDict[props.priority]} className="self-start mt-1.5">{props.priority}</Badge>
                 </div>
                 <div className="flex flex-row space-x-1.5 items-center">
                     <MapPin className="w-5"/>
-                    <p className="text-muted-foreground">Danang International Airport</p>
+                    <p className="text-muted-foreground">{props.location}</p>
                 </div>
                 <div className="h-5"></div>
                 <div className="flex flex-row space-x-5 items-center">
                     <div className="flex flex-row space-x-1.5 items-center">
-                        <PlaneTakeoff className="w-5" />
-                        <p className="text-foreground">Transport</p>
+                        <div className="w-5">{props.icon}</div>
+                        <p className="text-foreground">{props.category}</p>
                     </div>
                     <div className="flex flex-row space-x-1.5 items-center">
                         <AlarmClock className="w-5"/>
-                        <p className="text-foreground">14:30 pm</p>
+                        <p className="text-foreground">{props.time}</p>
                     </div>
                 </div>
             </CardContent>
