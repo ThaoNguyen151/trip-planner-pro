2c727b9dc5336808cac6acc7e82298d598ec2777
chihoang16 <149895979+hoangchi16@users.noreply.github.com>
2026-05-24 18:36:07 +0700

    feat: added empty trip error page and got title from trip store

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -1,8 +1,9 @@
 import {Calendar} from 'lucide-react';
+import { useTripStore } from '@/stores';
 
 export function DashboardHeader() {
     return(<div>
-        <h1 className="text-foreground text-3xl font-semibold">Da Nang Family Trip</h1>
+        <h1 className="text-foreground text-3xl font-semibold">{useTripStore.getState().trips[0].title}</h1>
         <div className='flex flex-row space-x-1.5 items-center pt-0.5'>
             <Calendar className='w-3 h-3' />
             <p className='text-muted-foreground font-bold'>Aug 15 - Aug 22, 2024</p>
--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,11 +1,25 @@
 import { DashboardHeader } from "@/components/dashboard/DashboardHeader";
 import { DisplayArea } from "@/components/dashboard/DisplayArea";
+import { useTripStore } from "@/stores";
 // import { ProgressCard } from "@/components/dashboard/ProgressCard";
 /** Dashboard — nội dung sẽ bổ sung sau. */
 export default function DashboardPage() {
-  return (
+  const allTrips = useTripStore.getState().trips;
+  const handleAddTrip = useTripStore.getState().addTrip;
+  if (allTrips.length === 0) {
+    return (
+      <div>
+        <h1>No trips found</h1>
+        <p>No trip details to display</p>
+        <button className="bg-amber-500" onClick={() => handleAddTrip("Da Nang Family Trip")}>Add Trip</button>
+      </div>
+    )
+  } else {
+    return (
     <div>
       <DashboardHeader></DashboardHeader>
       <DisplayArea></DisplayArea>
     </div>)
+  }
+  
 }
