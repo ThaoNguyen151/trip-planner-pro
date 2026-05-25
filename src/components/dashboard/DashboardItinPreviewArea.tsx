fa84e1424ab979c3fb40be93b2d908fab0670f17
chihoang16 <149895979+hoangchi16@users.noreply.github.com>
2026-05-26 00:40:45 +0700

    fix: update icon for itinerary preview

--- src/components/dashboard/DashboardItinPreviewArea.tsx
+++ src/components/dashboard/DashboardItinPreviewArea.tsx
@@ -1,12 +1,14 @@
 import type { ItineraryActivity } from "@/types";
 import DashboardItinPreviewCard from "./DashboardItinPreviewCard";
-import { Plane, Utensils, Hotel, Glasses, Clock} from "lucide-react";
+import { Plane, Utensils, Hotel, Glasses, Clock, ShoppingBag, CircleEllipsis} from "lucide-react";
 
 const iconDict = {
     Transport: <Plane></Plane>, 
     Lodging: <Hotel></Hotel>,
     Sightseeing: <Glasses></Glasses>,
-    Dining: <Utensils></Utensils>
+    Dining: <Utensils></Utensils>,
+    Shopping: <ShoppingBag></ShoppingBag>,
+    Other: <CircleEllipsis></CircleEllipsis>
 }
 
 interface DashboardItinPreviewAreaProps {
