e08937b1f1fd08dabb8077e98fd1b5e5c26b510e
chihoang16 <149895979+hoangchi16@users.noreply.github.com>
2026-05-25 12:49:15 +0700

    fix: reformat header text

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -4,7 +4,7 @@ import { useTripStore } from '@/stores';
 export function DashboardHeader() {
     const tripName = useTripStore((state) => state.trips[0].title)
     return(<div>
-        <h1 className="text-foreground text-3xl font-semibold">{tripName}</h1>
+        <h1 className="text-2xl font-bold tracking-tight text-foreground md:text-4xl">{tripName}</h1>
         <div className='flex flex-row space-x-1.5 items-center pt-0.5'>
             <Calendar className='w-3 h-3' />
             <p className='text-muted-foreground font-bold'>Aug 15 - Aug 22, 2024</p>
