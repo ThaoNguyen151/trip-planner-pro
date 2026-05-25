d8a57d310a479553f29cebc8af17df23631d5825
Daisy <104750729+ThaoAnna@users.noreply.github.com>
2026-05-25 20:50:55 +0700

    build: import activetripimport in packagePage
    build: import activetripimport in packagePage

--- src/pages/packing/PackingPage.tsx
+++ src/pages/packing/PackingPage.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import FilterDropdown from "../../components/package/FilterDropdown";
 import CategoryCard from "../../components/package/CategoryCard";
 import { usePackingStore } from "@/stores/usePackingStore";
+import { ActiveTripHeader } from "@/components/shared/ActiveTripHeader";
 
 export default function PackingPage() {
   const { categories, filters, setFilter, unpackAll } = usePackingStore()
